5f7bde725269082b4b10427c839fd1e067329465
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2022-12-09 10:03:58 +0200

    task-1-2-lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,53 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(galleryItems);
 
-// console.log(galleryItems);
+let modalImage;
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onImageClick);
 
-const galleryContainer = document.querySelector(".gallery");
-const galleryMarkup = createGalleryMarkup(galleryItems);
+createMarkUp();
 
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+function createMarkUp() {
+  const markup = galleryItems.reduce(
+    (acc, { preview, original, description }) => {
+      acc += `<div class="gallery__item">
+                  <a class="gallery__link" href="${original}">
+                    <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                    />
+                  </a>
+                </div>`;
+      return acc;
+    },
+    ""
+  );
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
 
-console.log(createGalleryMarkup(galleryItems));
-function createGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ original, preview, description }) => {
-      return `
-     <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-             <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-             />
-        </a>
-     </div>`;
-    })
-    .join("");
-  //   console.log(markup);
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") return;
+  const item = evt.target;
+  onImageShow(item.dataset.source, item.alt);
+}
+
+function onImageShow(imageSrc, imageAlt) {
+  modalImage = basicLightbox.create(
+    `<img src="${imageSrc}" alt="${imageAlt}">`,
+    {
+      onShow: () => window.addEventListener("keydown", onImageEscClose),
+    },
+    {
+      onClose: () => window.removeEventListener("keydown", onImageEscClose),
+    }
+  );
+  modalImage.show();
+}
+
+function onImageEscClose(evt) {
+  if (evt.code === "Escape") modalImage.close();
 }
